66e99f424fbbe7b6e0f0ccd2e23c411662b3d397
Sebastien Bovo <sebastienbovo@outlook.com>
2018-06-26 11:21:59 +0200

    Use UWP APIs from a standard Unity EXE

---
+++ UWPAPIsUsedFromUnityEXEApp/UnityEXEApp/Assets/ClickToUseDll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class ClickToUseDll : MonoBehaviour
+{
+
+    [DllImport("dotNETClassLibraryUsingUWPAPIs")]
+    extern static string Notify(string toastTitle, string toastContent);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            string output = Notify("Toast title",
+                "Unity toast sent at " + System.DateTime.Now.ToLongTimeString());
+        }
+    }
+}
---
+++ UWPAPIsUsedFromUnityEXEApp/dotNETClassLibraryUsingUWPAPIs/dotNETClassLibraryUsingUWPAPIs/NotificationHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace dotNETClassLibraryUsingUWPAPIs
+{
+    class NotificationHelper
+    {
+        [DllExport(CallingConvention.StdCall)]
+        public static string Notify(string toastTitle, string toastContent)
+        {
+            try
+            {
+                string xml = @"<toast activationType='foreground'>
+                                        <visual>
+                                                 <binding template='ToastGeneric'>
+                                                 </binding>
+                                        </visual>
+                               </toast>";
+
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xml);
+
+                var binding = doc.SelectSingleNode("//binding");
+
+                var el = doc.CreateElement("text");
+                el.InnerText = toastTitle;
+
+                binding.AppendChild(el);
+
+                el = doc.CreateElement("text");
+                el.InnerText = toastContent;
+                binding.AppendChild(el);
+
+                var toast = new ToastNotification(doc);
+
+                ToastNotificationManager.CreateToastNotifier().Show(toast);
+            }
+            catch (AggregateException ex)
+            {
+                return "Exception:" + ex.ToString() + "exMessage:" + ex.Message;
+            }
+            return "Ok";
+        }
+    }
+}
