62300f4e69239a7a42811baf16cef79a02fb2ecc
Matteo Pagani <info@qmatteoq.com>
2018-07-18 23:37:07 +0200

    Added Package Support Framework sample

---
+++ Blog-PSF/PSFDemo/PSFDemo/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace PSFDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnReadFile(object sender, RoutedEventArgs e)
+        {
+            string filePath = $"{Environment.CurrentDirectory}/app.json";
+            string json = File.ReadAllText(filePath);
+            Config config = JsonConvert.DeserializeObject<Config>(json);
+            AppName.Text = config.AppName;
+            Version.Text = config.Version;
+        }
+
+        private void OnWriteFile(object sender, RoutedEventArgs e)
+        {
+            Config config = new Config
+            {
+                AppName = AppName.Text,
+                Version = Version.Text
+            };
+
+            string json = JsonConvert.SerializeObject(config);
+
+            string filePath = $"{Environment.CurrentDirectory}/app.json";
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
