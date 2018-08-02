3905cc1571c9c52d47396c95c355941767ec2aaf
Matteo Pagani <matteo.pagani@microsoft.com>
2018-08-02 16:50:31 +0200

    Added sample for using a Windows Runtime Component in a .NET application

---
+++ Blog-WinRTComponent/WinRTComponent/Notifications/NotificationHelper.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "NotificationHelper.h"
+
+using namespace Notifications;
+using namespace Platform;
+using namespace Windows::Data::Xml::Dom;
+using namespace Windows::UI::Notifications;
+
+NotificationHelper::NotificationHelper()
+{
+}
+
+void NotificationHelper::ShowNotification(Platform::String^ title, Platform::String^ content)
+{
+	Platform::String^ xml = "<toast><visual><binding template='ToastGeneric'><text>" + title +" </text><text>" + content +"</text></binding></visual></toast>";
+	XmlDocument^ doc = ref new XmlDocument();
+	doc->LoadXml(xml);
+
+
+	ToastNotification^ toast = ref new ToastNotification(doc);
+	ToastNotificationManager::CreateToastNotifier()->Show(toast);
+}
---
+++ Blog-WinRTComponent/WinRTComponent/Notifications/NotificationHelper.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+namespace Notifications
+{
+    public ref class NotificationHelper sealed
+    {
+    public:
+		NotificationHelper();
+		void ShowNotification(Platform::String^ title, Platform::String^ content);
+    };
+}
