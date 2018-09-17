02be6485b70ae476541260e2d525b92262e46b5c
Matteo Pagani <matteo.pagani@microsoft.com>
2018-09-17 12:08:05 +0200

    Added MSIX Modification Packages sample

---
+++ Blog-MSIXModificationPackages/ModificationPackage/ModificationPackage/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModificationPackage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnReadTextFile(object sender, RoutedEventArgs e)
+        {
+            string path = $"{Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86)}\\ModificationPackage\\test.txt";
+            FilePath.Text = path;
+            if (File.Exists(path))
+            {
+                string content = File.ReadAllText(path);
+                FileContent.Text = content;
+            }
+            else
+            {
+                FileContent.Text = "File is missing";
+            }
+        }
+    }
+}
